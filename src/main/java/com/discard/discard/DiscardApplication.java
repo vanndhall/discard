349e80e504141b33dec916b74997099f046e8404
vanndhall <szymon.mantey@wp.pl>
2019-09-01 09:35:36 +0200

    Initial commit

---
+++ src/main/java/com/discard/discard/DiscardApplication.java
@@ -0,0 +1,13 @@
+package com.discard.discard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscardApplication.class, args);
+	}
+
+}
